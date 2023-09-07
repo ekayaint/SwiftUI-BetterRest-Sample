6b384d1af0152acdf9ed8ba3c219f7557d3eb91c
ekayaint <ekayaint@gmail.com>
2023-09-07 17:38:42 +0300

    Initial Commit

---
+++ SwiftUI-BetterRest-Sample/SwiftUI_BetterRest_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BetterRest_SampleApp.swift
+//  SwiftUI-BetterRest-Sample
+//
+//  Created by ekayaint on 7.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BetterRest_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
